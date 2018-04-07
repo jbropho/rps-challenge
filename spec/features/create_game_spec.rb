cde02941d766d7d649c8cf1062bb038822222b9a
Jordan Brooks <jordanbrooks@MacBook-Pro-68.lan>
2018-04-07 15:08:29 +0100

    extend feature test coverage

--- spec/features/create_game_spec.rb
+++ spec/features/create_game_spec.rb
@@ -3,12 +3,19 @@ RSpec.feature "Attack a player", :type => :feature do
      visit('/')
      expect(page).to have_text('Player One:')
    end
-   scenario 'User inputs player names' do
+   scenario 'User inputs two player names' do
      visit('/')
      fill_in "player_one", :with => "Biker"
      fill_in "player_two", :with => "Bitterman"
      click_button "Submit"
      expect(page).to have_text("Biker")
      expect(page).to have_text("Bitterman")
+   end
+
+   scenario 'User leaves player2 as computer default' do
+     visit('/')
+     fill_in "player_one", :with => "Biker"
+     click_button "Submit"
+     expect(page).to have_text("computer")
    end
 end
