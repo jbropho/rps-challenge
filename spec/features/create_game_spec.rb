7f259c1297966dff4b5e7c06f104432020603f93
Jordan Brooks <jordanbrooks@MacBook-Pro-68.lan>
2018-04-07 15:05:16 +0100

    pass initial feature test for user submits player names

--- spec/features/create_game_spec.rb
+++ spec/features/create_game_spec.rb
@@ -1,6 +1,14 @@
 RSpec.feature "Attack a player", :type => :feature do
-   scenario "User visits the home page" do
+   scenario 'User visits the home page' do
      visit('/')
      expect(page).to have_text('Player One:')
+   end
+   scenario 'User inputs player names' do
+     visit('/')
+     fill_in "player_one", :with => "Biker"
+     fill_in "player_two", :with => "Bitterman"
+     click_button "Submit"
+     expect(page).to have_text("Biker")
+     expect(page).to have_text("Bitterman")
    end
 end
