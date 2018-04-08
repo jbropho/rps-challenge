52ca4aee5db09e9c4eaa5fc93a384bc2786beff9
Jordan Brooks <jordanbrooks@MacBook-Pro-68.lan>
2018-04-08 18:14:42 +0100

    refactor app.rb to accomdate changes to Player/AiOpponent classes

--- app.rb
+++ app.rb
@@ -1,6 +1,8 @@
 require 'sinatra/base'
 require './lib/player'
 require './lib/game'
+require './lib/ai_opponent'
+require 'pry'
 
 class RPSapp < Sinatra::Base
   enable :sessions
@@ -9,22 +11,20 @@ class RPSapp < Sinatra::Base
     erb :index
   end
 
-  post '/names' do
+  post '/single-player-names' do
     session[:player_one] = Player.new(params[:player_one])
-    session[:player_two] = Player.new(params[:player_two])
     redirect to '/play'
   end
 
   get '/play' do
     @player_one = session[:player_one]
-    @player_two = session[:player_two]
     erb :play
   end
 
   post '/play' do
     @player_one = session[:player_one]
-    @player_two = session[:player_two]
-    @game = Game.new(@player_one => params[:p1].to_sym, @player_two => params[:p2].to_sym)
+    @player_one.choice = params[:p1].to_sym
+    @game = Game.new(@player_one, AiOpponent.new)
     @game.determine_winner
     erb :play
   end
