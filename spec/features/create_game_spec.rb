2073a978fc6a63573b6235e7815083d033938858
Jordan Brooks <jordanbrooks@MacBook-Pro-68.lan>
2018-04-08 12:54:16 +0100

    refactor tests after removing logic from play.erb

--- spec/features/create_game_spec.rb
+++ spec/features/create_game_spec.rb
@@ -7,18 +7,7 @@ RSpec.feature "Initialise a game", :type => :feature do
    scenario 'User inputs two player names' do
      visit('/')
      fill_in "player_one", :with => "Biker"
-     fill_in "player_two", :with => "Bitterman"
      click_button "Submit"
      expect(page).to have_text("Biker")
-     expect(page).to have_text("Bitterman")
-     expect(page).not_to have_field('#computer-input')
-   end
-
-   scenario 'User leaves player2 as computer default' do
-     visit('/')
-     fill_in "player_one", :with => "Biker"
-     click_button "Submit"
-     expect(page).to have_text('Computer')
-     expect(page).to have_selector("input#computer", :visible => false)
    end
 end
