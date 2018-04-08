de795a9dd64497940aaa91620fa4fcd926b88b26
Jordan Brooks <jordanbrooks@MacBook-Pro-68.lan>
2018-04-08 18:22:38 +0100

    refactor Game class unit tests to accomdate changes to class logic

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,25 +1,26 @@
 require 'game'
 
 describe Game do
-  subject(:hossman) { double(:name => :Hossman)}
-  subject(:cadaver) { double(:name => :Cadaver)}
-  subject(:bitterman) { double(:name => :Bitterman)}
+  subject(:hossman) { double(:name => :Hossman, :choice => :paper)}
+  subject(:cadaver) { double(:name => :Cadaver, :choice => :rock)}
+  subject(:bitterman) { double(:name => :Bitterman, :choice => :scissors)}
+  subject(:grunt) { double(:name => :Grunt, :choice => :rock)}
 
   describe '.determine_winner' do
     it 'returns a winner paper > rock' do
-      game = Game.new({ hossman => :paper, cadaver => :rock})
+      game = Game.new(hossman, cadaver)
       expect(game.determine_winner).to eq :Hossman
     end
-    it 'returns a winner paper > scissors' do
-      game = Game.new({cadaver => :scissors, hossman => :paper })
-      expect(game.determine_winner).to eq :Cadaver
-    end
     it 'returns a winner scissors > paper' do
-      game = Game.new({ bitterman => :scissors, cadaver => :paper})
+      game = Game.new(bitterman, hossman)
       expect(game.determine_winner).to eq :Bitterman
     end
+    it 'returns a winner rock > scissors' do
+      game = Game.new(cadaver, bitterman)
+      expect(game.determine_winner).to eq :Cadaver
+    end
     it 'returns String(nobody) when both players make same choice' do
-      game = Game.new({ hossman => :rock, cadaver => :rock})
+      game = Game.new(grunt, cadaver)
       expect(game.determine_winner).to eq 'nobody'
     end
   end
