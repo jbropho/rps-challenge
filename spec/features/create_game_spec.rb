484ca3d67d9d3a41598ccdc9a447c00e49d0232e
Jordan Brooks <jordanbrooks@MacBook-Pro-68.lan>
2018-04-08 12:59:46 +0100

    remove redundant test after refactor

--- spec/features/create_game_spec.rb
+++ spec/features/create_game_spec.rb
@@ -3,11 +3,4 @@ RSpec.feature "Initialise a game", :type => :feature do
      visit('/play_ai')
      expect(page).to have_text('Player One:')
    end
-
-   scenario 'User inputs two player names' do
-     visit('/play_ai')
-     fill_in "player_one", :with => "Biker"
-     click_button "Submit"
-     expect(page).to have_text("Biker")
-   end
 end
